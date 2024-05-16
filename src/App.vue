2a893145c5e8d4c010bf9cc5ecbce3bf6f8b4757
samsul580 <samsulshekh2019@gmail.com>
2024-05-16 17:10:27 +0600

    prac: reavtive

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import HelloWorld from './components/HelloWorld.vue'
 import GlobalStyle from './components/GlobalStyle.vue'
 import LocalStyle from './components/LocalStyle.vue'
 import MyEvent from './components/MyEvent.vue'
+import ReactiveComponent from './components/ReactiveComponent.vue'
 </script>
 <template>
   <HelloWorld />
   <GlobalStyle />
   <LocalStyle />
   <MyEvent />
+  <ReactiveComponent />
 </template>
 <style scoped></style>
