92dfced393648e65387794ac3fb1c9e074c7c9a8
samsul580 <samsulshekh2019@gmail.com>
2024-05-16 11:31:49 +0600

    Text Interpolation

--- src/components/AnotherComponent.vue
+++ src/components/AnotherComponent.vue
@@ -1,3 +1,17 @@
+<script setup>
+const myMessage = 'Yellow World'
+const herMessage = 'White World'
+const num = 100
+function add(a, b) {
+  return a + b
+}
+</script>
+
 <template>
   <h1>Nested Component</h1>
+  <h2>My Message: {{ myMessage }}</h2>
+  <h2>Her Message: {{ herMessage }}</h2>
+  <h3>Number: {{ num }}</h3>
+  <h3>Number + 50: {{ num + 50 }}</h3>
+  <h3>Add function call: {{ add(200, 300) }}</h3>
 </template>
